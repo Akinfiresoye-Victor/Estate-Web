ef3a215e5c654ca4a4d8ea679fc39563141c6101
Akinfiresoye Victor <eninlavictor30@gmail.com>
2025-09-26 15:05:39 +0100

    Added feedback section

--- estate_web/settings.py
+++ estate_web/settings.py
@@ -78,7 +78,7 @@ DATABASES = {
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
+EMAIL_PORT = 986
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = config('EMAIL_USER')
 EMAIL_HOST_PASSWORD = config('EMAIL_PASS')
