e38b426edb742da695e359e081553a6076e18162
Akinfiresoye Victor <eninlavictor30@gmail.com>
2025-10-14 16:46:09 +0100

    Fixed Message bug

--- estate_web/asgi.py
+++ estate_web/asgi.py
@@ -8,7 +8,7 @@ from django.core.asgi import get_asgi_application
 from estate import routing
 
 # this is used to set up the django settings model if it hasnt been setup already this should come before initializing the asgi application
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'movie.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'estate_web.settings')
 
 #setting our django instance up it must be called after we already set our default environment
 django_asgi_app = get_asgi_application()
