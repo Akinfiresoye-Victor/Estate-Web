dc185c17046503aacfa5277da41edca5b3b05fe3
Akinfiresoye Victor <eninlavictor30@gmail.com>
2025-10-14 18:35:18 +0100

    .

--- estate/consumer.py
+++ estate/consumer.py
@@ -1,9 +1,6 @@
 import json
 from channels.generic.websocket import AsyncWebsocketConsumer
 from channels.db import database_sync_to_async
-from .models import Room, Messages
-
-
 
 class ChatConsumer(AsyncWebsocketConsumer):
     async def connect(self):
@@ -11,32 +8,37 @@ class ChatConsumer(AsyncWebsocketConsumer):
         await self.channel_layer.group_add(self.room_name, self.channel_name)
         await self.accept()
 
-
     async def disconnect(self, close_code):
         await self.channel_layer.group_discard(self.room_name, self.channel_name)
-        
 
     async def receive(self, text_data):
-            text_data_json=json.loads(text_data)
-            message=text_data_json
-            event={
-                'type': 'send_message',
-                'message':message,
-            }
-            await self.channel_layer.group_send(self.room_name, event)
-    
+        text_data_json = json.loads(text_data)
+        message = text_data_json
+        event = {
+            'type': 'send_message',
+            'message': message,
+        }
+        await self.channel_layer.group_send(self.room_name, event)
+
     async def send_message(self, event):
-        data=event['message']
-        await self.create_message(data= data)
-        response_data={
+        data = event['message']
+        await self.create_message(data=data)
+        response_data = {
             'sender': data['sender'],
             'message': data['message']
         }
         await self.send(text_data=json.dumps({'message': response_data}))
-    
+
     @database_sync_to_async
     def create_message(self, data):
-        get_room_by_name=Room.objects.get(room_name=data['room_name'])
-        if not Messages.objects.filter(message=data['message']).exists():
-            new_message= Messages(room=get_room_by_name, sender=data['sender'], message=data['message'])
-            new_message.save()
+        # Lazy import to avoid AppRegistryNotReady
+        from .models import Room, Messages  
+
+        try:
+            room = Room.objects.get(room_name=data['room_name'])
+        except Room.DoesNotExist:
+            return  # optionally handle room not found
+
+        # Only create message if it doesn't exist
+        if not Messages.objects.filter(message=data['message'], room=room).exists():
+            Messages.objects.create(room=room, sender=data['sender'], message=data['message'])
