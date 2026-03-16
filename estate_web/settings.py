9835ebffdd98db038f6a411620b346ce14d6d9b3
Akinfiresoye Victor <eninlavictor30@gmail.com>
2026-03-16 21:37:44 +0100

    still setting domain

--- estate_web/settings.py
+++ estate_web/settings.py
@@ -12,7 +12,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY
 SECRET_KEY = config('SECRET_KEY')
 DEBUG = config('DEBUG', default=False, cast=bool)
-ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '').split(',')
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=Csv())
 
 CSRF_TRUSTED_ORIGINS = [
     'https://estate-web-sufx.onrender.com',
