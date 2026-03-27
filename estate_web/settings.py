d5c38c6ae67a6a0c9cba5193e62b535f96ff215b
Akinfiresoye Victor <eninlavictor30@gmail.com>
2026-03-27 23:16:34 +0100

    Updated Security Measures

--- estate_web/settings.py
+++ estate_web/settings.py
@@ -114,7 +114,7 @@ else:
 EMAIL_BACKEND = 'zoho_zeptomail.backend.zeptomail_backend.ZohoZeptoMailEmailBackend'
 ZOHO_ZEPTOMAIL_API_KEY_TOKEN = config('ZEPTOMAIL_API_TOKEN')
 ZOHO_ZEPTOMAIL_HOSTED_REGION = 'zeptomail.zoho.com' 
-DEFAULT_FROM_EMAIL = 'contact@estatewebng.com'
+DEFAULT_FROM_EMAIL = config('DEFAULT_FROM_EMAIL')
 
 SOCIALACCOUNT_PROVIDERS = {
     'google': {
